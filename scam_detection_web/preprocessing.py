c61406549e257ca2bdb62563a2bf6cf833c9feb1
Akash Masanamuthu <akashmasanamuthu@gmail.com>
2026-02-16 10:50:07 +0530

    Update preprocessing.py

--- scam_detection_web/preprocessing.py
+++ scam_detection_web/preprocessing.py
@@ -3,6 +3,17 @@ import string
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 
+# Download required NLTK data safely
+def download_nltk_resources():
+    resources = ["stopwords", "punkt", "wordnet"]
+    for resource in resources:
+        try:
+            nltk.data.find(f"corpora/{resource}")
+        except LookupError:
+            nltk.download(resource)
+
+download_nltk_resources()
+
 stop_words = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
 
